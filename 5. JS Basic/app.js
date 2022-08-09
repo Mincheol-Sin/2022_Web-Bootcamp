87ac8b25f68302c85ea6d19c57e31527b70b9bae
Mincheol Sin <cheole720@gmail.com>
2022-08-09 22:35:39 +0900

    Feat: 5. JS Basic 코스 시작

---
+++ 5. JS Basic/app.js
@@ -0,0 +1,9 @@
+// 변수 선언 키워드 let
+
+let age = 32;
+let greetingText = "Hi, I'm Max!!!";
+alert(greetingText);
+alert(greetingText);
+greetingText = "Hi, I am really max!";
+alert(greetingText);
+alert(age);
