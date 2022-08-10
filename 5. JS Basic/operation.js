0a83a8eabaf11a6fdc567303e3564367b49d468a
Mincheol Sin <cheole720@gmail.com>
2022-08-10 16:30:46 +0900

    Feat: 수학 연산 이론 학습

---
+++ 5. JS Basic/operation.js
@@ -0,0 +1,22 @@
+// Math operations
+
+console.log(10 + 4);
+console.log(10 - 4);
+console.log(10 * 4);
+console.log(10 / 4);
+console.log(10 % 4); // Remainder: 2
+console.log(10 % 3); // Remainder: 1
+
+console.log(10 + 3 - 5 * 10); // -37
+console.log((10 + 3 - 5) * 10); // 80
+
+let result = (10 + 3 - 5) * 10;
+result = 10 * 4;
+result++; // result = result + 1
+result--; // result = result - 1
+
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result /= 5; // result = result / 5
+result *= 5; // result = result * 5
+console.log(result);
