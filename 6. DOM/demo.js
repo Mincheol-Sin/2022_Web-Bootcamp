26681954718be56afa65ca85f15d5a1acad50a6e
Mincheol Sin <cheole720@gmail.com>
2022-08-12 19:43:05 +0900

    Feat: DOM을 활용한 자바스크립트 실전예제2 학습

--- 6. DOM/demo.js
+++ 6. DOM/demo.js
@@ -1,15 +1,15 @@
 // 1. Input 요소와 남아있는 글자수를 나타내는 요소에 접근하기
-let productNameInputElement = document.getElementById("product-name");
-let remainningCharsElement = document.getElementById("remaining-chars");
+const productNameInputElement = document.getElementById("product-name");
+const remainningCharsElement = document.getElementById("remaining-chars");
 
-let maxAllowedChars = productNameInputElement.maxLength;
+const maxAllowedChars = productNameInputElement.maxLength;
 
 // 3. 발생할 이벤트를 함수로 선언하기
 function updateRemainingCharacters(event) {
-  let enteredText = event.target.value; // 입력한 값
-  let enteredTextLength = enteredText.length; // 입력한 값의 글자수
+  const enteredText = event.target.value; // 입력한 값
+  const enteredTextLength = enteredText.length; // 입력한 값의 글자수
 
-  let remainingCharacters = maxAllowedChars - enteredTextLength; // 남은 글자수
+  const remainingCharacters = maxAllowedChars - enteredTextLength; // 남은 글자수
 
   remainningCharsElement.textContent = remainingCharacters; // 남은 글자수로 콘텐츠 변환
 }
