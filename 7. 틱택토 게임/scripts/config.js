b6c5e14871d823254d91066eab4c37fb2c33d712
Mincheol Sin <cheole720@gmail.com>
2022-08-14 22:11:12 +0900

    Feat: 양식 제출 처리 및 사용자 입력 확인 기능 구현

--- 7. 틱택토 게임/scripts/config.js
+++ 7. 틱택토 게임/scripts/config.js
@@ -6,4 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault(); // 서버에 데이터 전송하는 것을 막아서 페이지 리로드 되는 것 방지
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim();
+
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
 }
